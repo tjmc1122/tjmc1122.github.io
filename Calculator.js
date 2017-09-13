6337adfd2600dc70fe2617d4945098e33338fc17
tajhai <tajhmc25@gmail.com>
2017-09-13 13:04:02 -0400

    Now accepts valid inputs and updates text box

--- Calculator.js
+++ Calculator.js
@@ -1,15 +1,45 @@
-blinker = document.getElementById("blinker");
+
+var textBox = document.getElementById("textBox").innerHTML;
+var entered_text = "";
+var ops = ["+","-","/","*","="];
 zeroOpacity= false;
+
 function blink(opacity){
+    blinker = document.getElementById("blinker");
+    //console.log(blinker);
     if(!zeroOpacity){
         blinker.style.opacity = "0";
         zeroOpacity = true;
     }
     else{
-        blinker.style.opacity = "1";
+        blinker.style.opacity = "1";        
         zeroOpacity =  false;
     }
+ //console.log(blinker.style.opacity);
 }
 blinkId = setInterval(function m(){blink(zeroOpacity);},500);
 
-blinkHolder = document.getElementById("head_display");
+document.onkeypress=verifyInput;
+
+function verifyInput(e){
+    x = new Number(e.key);
+    console.log(x);
+    if(!Object.is(x.valueOf(),NaN)){
+       Write(e.key);  
+    }
+    else if(ops.includes(e.key)){
+        if(e.key != "="){Write(e.key);}
+        else{sum(entered_text)};
+    }
+    
+    
+}
+
+function sum(expression){
+    console.log(expression);
+}
+function Write(num){
+    x = document.getElementById("textBox");
+    entered_text += num;
+    x.innerHTML = entered_text + textBox;    
+}
